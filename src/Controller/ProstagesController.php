a7eb8c3f98bec53b1d312e3cf3fced5efc8d7617
Tiago Buil <tbuil@iutbayonne.univ-pau.fr>
2022-01-11 14:32:06 +0100

    creation du controller

---
+++ src/Controller/ProstagesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProstagesController extends AbstractController
+{
+
+    public function index(): Response
+    {
+        return $this->render('prostages/index.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+
+    public function entreprises(): Response
+    {
+        return $this->render('prostages/entreprises.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+
+    public function formations(): Response
+    {
+        return $this->render('prostages/formations.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+
+    public function stages($id): Response
+    {
+        return $this->render('prostages/stages.html.twig', [
+            'id' => $id,
+        ]);
+    }
+}
