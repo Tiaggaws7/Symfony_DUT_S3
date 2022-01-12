c9ad3b63544575f28f87f898dd701abd0a3c5553
Tiago Buil <tbuil@iutbayonne.univ-pau.fr>
2022-01-12 11:29:33 +0100

    creation des données test pour stage

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,28 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Stage;
+use App\Entity\Formation;
+use App\Entity\Entreprise;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
-        // $manager->persist($product);
+
+        //Création d'un générateur de données Faker
+        $faker = \Faker\Factory::create('fr_FR');
+
+        for ($i=0; $i < 10; $i++) { 
+            $fixturesStage = new Stage();
+            $fixturesStage->setTitre($faker->realText($maxNbChars = 28, $indexSize = 2));
+            $fixturesStage->setMission($faker->realText($maxNbChars = 100, $indexSize = 2));
+            $fixturesStage->setEmail($faker->freeEmail);
+    
+            $manager->persist($fixturesStage);
+        }
+
 
         $manager->flush();
     }
