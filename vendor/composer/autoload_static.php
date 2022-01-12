5ac8d9e27f019933cc584ac4136b281d3b12b365
Tiago Buil <tbuil@iutbayonne.univ-pau.fr>
2022-01-12 10:28:15 +0100

    installation de faker

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -125,6 +125,10 @@ class ComposerStaticInitfc30bc7f4a0780ae6443cc390fbe1519
         array (
             'Laminas\\Code\\' => 13,
         ),
+        'F' => 
+        array (
+            'Faker\\' => 6,
+        ),
         'E' => 
         array (
             'Egulias\\EmailValidator\\' => 23,
@@ -483,6 +487,10 @@ class ComposerStaticInitfc30bc7f4a0780ae6443cc390fbe1519
         'Laminas\\Code\\' => 
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-code/src',
+        ),
+        'Faker\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/fzaninotto/faker/src/Faker',
         ),
         'Egulias\\EmailValidator\\' => 
         array (
